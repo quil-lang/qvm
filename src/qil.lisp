28b5bf4dc057da50cf2e1bdb23b8e8d0c516d981
Robert Smith <robert@rigetti.com>
2016-05-19 16:21:01 -0700

    add #'PARSE-QIL-INSTRUCTION

--- src/qil.lisp
+++ src/qil.lisp
@@ -6,16 +6,22 @@
 
 ;;;; QIL parsing. For now, this is just S-expressions.
 
+(defun parse-qil-instruction (string)
+  (with-standard-io-syntax
+    (let ((*read-eval* nil))
+      (read-from-string string nil nil))))
+
 (defun slurp-forms (stream)
   "Slurp all of the forms of the stream STREAM into a list."
-  (loop :with eof := (gensym "EOF")
-        :for form := (read stream nil eof nil) :then (read stream nil eof nil)
-        :until (eq eof form)
-        :collect form))
+  (with-standard-io-syntax
+    (let ((*read-eval* nil))
+      (loop :with eof := (gensym "EOF")
+            :for form := (read stream nil eof nil) :then (read stream nil eof nil)
+            :until (eq eof form)
+            :collect form))))
 
-(defun read-qil-file (filespec)
-  (let ((*read-eval* nil))
-    (with-open-file (s filespec :direction ':input
-                                :if-does-not-exist ':error)
-      (let ((*package* (find-package :qvm)))
-        (slurp-forms s)))))
+(defun read-qil-file (filespec)  
+  (with-open-file (s filespec :direction ':input
+                              :if-does-not-exist ':error)
+    (let ((*package* (find-package :qvm)))
+      (slurp-forms s))))
