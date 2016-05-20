f1c18601b27c924eeac35b6a50f345dfefa746c6
Robert Smith <robert@rigetti.com>
2016-05-20 11:18:44 -0700

    integrate server with pre-existing cmdline app

--- app-src/entry-point.lisp
+++ app-src/entry-point.lisp
@@ -18,7 +18,10 @@
   (terpri *trace-output*))
 
 (defun show-help ()
-  (format t "Usage: ~A <num-qubits> <qil-file> [<options>...]~%~%" *program-name*)
+  (format t "Run QIL file:~%")
+  (format t "    ~A <num-qubits> <qil-file> [<options>...]~%~%" *program-name*)
+  (format t "Start QVE server:~%")
+  (format t "    ~A~%~%" *program-name*)
   (command-line-arguments:show-option-help *option-spec* :sort-names t))
 
 (defmacro with-timing ((var) &body body)
@@ -28,41 +31,41 @@
          (setf ,var (round (* 1000 (- (get-internal-real-time) ,start))
                            internal-time-units-per-second))))))
 
-(defun format-complex (c)
-  (cond
-    ((zerop (imagpart c))
-     (format nil "~F" (realpart c)))
-    ((plusp (imagpart c))
-     (format nil "~F+~Fi" (realpart c) (imagpart c)))
-    ((minusp (imagpart c))
-     (format nil "~F-~Fi" (realpart c) (abs (imagpart c))))))
+(defun expectation-value (qvm qubits)
+  (flet ((parity (i)
+           (if (oddp (logcount i))
+               -1
+               1)))
+    (loop :for i :from 0
+          :for p :in (qvm::state-probabilities qvm (apply #'qvm::nat-tuple qubits))
+          :sum (* p (parity i)))))
 
-(defun process-options (qubits file &key help memory)
-  (when help
-    (show-help)
-    (uiop:quit))
-  
-  (setf qubits (parse-integer qubits :junk-allowed nil))
-  
-  (let (qvm program alloc-time exec-time)
-    (format-log "Allocating memory for QVM")
-    (with-timing (alloc-time)
-      (setf qvm (make-qvm qubits :classical-memory-size memory)))
-    (format-log "Allocation completed in ~D ms. Reading in program," alloc-time)
-    (setf program (read-qil-file file))
-    (format-log "Loading quantum program.")
-    (load-program qvm program)
-    (format-log "Executing quantum program.")
-    (setf *random-state* (make-random-state t)) ; Seed random.
-    (with-timing (exec-time)
-      (run qvm))
-    (format-log "Execution completed in ~D ms. Printing state." exec-time)
-    (when (<= qubits 5)
-      (format-log "Amplitudes: ~{~A~^, ~}" (map 'list 'format-complex (amplitudes qvm)))
-      (format-log "Probabilities: ~{~F~^, ~}" (map 'list 'probability (amplitudes qvm))))
-    (format-log "Classical memory (MSB -> LSB): ~v,'0B" (* 8 (classical-memory-size qvm)) (classical-memory qvm)))
-  
-  (uiop:quit))
+(defun run-experiment (num-qubits qubits-to-measure trials qil)
+  (let ((qvm (qvm:make-qvm num-qubits))
+        (stats (make-array (length qubits-to-measure) :initial-element 0))
+        timing)
+    (format-log "Running experiment with ~D trial~:P" trials)
+    ;; Direct wavefunction probability computation
+    #-ignore
+    (with-timing (timing)
+      (qvm:load-program qvm qil)
+      (qvm:run qvm)
+      (setf (aref stats 0)
+            (* trials (expectation-value qvm qubits-to-measure))))
+    ;; Sampling
+    #+ignore
+    (with-timing (timing)
+      (dotimes (i trials)
+        (qvm:load-program qvm qil)
+        (qvm:run qvm)
+        (multiple-value-bind (new-qvm cbits)
+            (qvm:parallel-measure qvm qubits-to-measure)
+          (setf qvm (qvm::reset new-qvm))
+          (map-into stats #'+ stats cbits))))
+    (format-log "Finished in ~D ms" timing)
+    (coerce stats 'list)))
+
+(defparameter *default-port* 5000)
 
 (defun slurp-stream (stream)
   (with-output-to-string (s)
@@ -73,34 +76,8 @@
 (defun keywordify (str)
   (intern (string-upcase str) :keyword))
 
-(defun run-experiment (num-qubits qubits-to-measure trials qil)
-  (let ((qvm (qvm:make-qvm num-qubits))
-        (stats (make-array (length qubits-to-measure) :initial-element 0))
-        timing)
-    (format-log "Running experiment with ~D trials" trials)
-    (with-timing (timing)
-      (qvm:load-program qvm qil)
-      (qvm:run qvm)
-      (print (qvm::amplitudes qvm))
-      (setf (aref stats 0)
-            (* trials (qvm::multi-qubit-probability qvm qubits-to-measure)))
-      #+ignore
-      (dotimes (i trials)
-        (qvm:load-program qvm qil)
-        (qvm:run qvm)
-        (multiple-value-bind (new-qvm cbits)
-            (qvm:parallel-measure qvm qubits-to-measure)
-          (setf qvm (qvm::reset new-qvm))
-          (map-into stats #'+ stats cbits))))
-    (format-log "Finished ~D trials in ~D ms" trials timing)
-    (coerce stats 'list)))
-
-(defun filt (isn)
-  (if (string-equal "RZ" (symbol-name (car isn)))
-      (list (car (second isn)))
-      nil))
-
-(defun %main (argv)
+(defun start-server ()
+  (format-log "Starting server on port ~D." *default-port*)
   (woo:run
    (lambda (env)
      (let ((body-stream (getf env :raw-body)))
@@ -116,12 +93,13 @@
                  (trials (gethash :TRIALS js))
                  (isns (gethash :QIL-INSTRUCTIONS js))
                  (qil (mapcar #'qvm::parse-qil-instruction isns)))
-            ;(format-log "HTTP Payload: ~S" data)
-            (format-log "  Qubits: ~A" qubits)
-            (format-log "  Measure: ~A" qubits-to-measure)
-            (format-log "  Trials: ~A" trials)
-            (let ((*print-pretty* nil))
-              (format-log "  QIL: ~S" (mapcan #'filt qil)))
+            (format-log "~D qubits, measuring ~A, over ~D trial~:P"
+                        qubits
+                        qubits-to-measure
+                        trials)
+            (let ((*print-pretty* nil)
+                  (*print-length* 8))
+              (format-log "QIL Code: ~S" qil))
             (let* ((counts (run-experiment qubits
                                            qubits-to-measure
                                            trials
@@ -130,27 +108,71 @@
                      (yason:with-output-to-string* (:indent t)
                        (yason:with-object ()
                          (yason:encode-object-element "statistics" counts)))))
-              `(200 (:content-type "text/plain") (,res-payload))))))))))
+              `(200 (:content-type "text/plain") (,res-payload))))))))
+   :port *default-port*))
 
-#+#:ignore
+(defun format-complex (c)
+  (cond
+    ((zerop (imagpart c))
+     (format nil "~F" (realpart c)))
+    ((plusp (imagpart c))
+     (format nil "~F+~Fi" (realpart c) (imagpart c)))
+    ((minusp (imagpart c))
+     (format nil "~F-~Fi" (realpart c) (abs (imagpart c))))))
+
+(defun process-options (qubits file &key help memory)
+  (when help
+    (show-help)
+    (uiop:quit))
+
+  (setf qubits (parse-integer qubits :junk-allowed nil))
+
+  (cond
+    (server (start-server))
+    (t
+     (let (qvm program alloc-time exec-time)
+       (format-log "Allocating memory for QVM")
+       (with-timing (alloc-time)
+         (setf qvm (make-qvm qubits :classical-memory-size memory)))
+       (format-log "Allocation completed in ~D ms. Reading in program," alloc-time)
+       (setf program (read-qil-file file))
+       (format-log "Loading quantum program.")
+       (load-program qvm program)
+       (format-log "Executing quantum program.")
+       (setf *random-state* (make-random-state t)) ; Seed random.
+       (with-timing (exec-time)
+         (run qvm))
+       (format-log "Execution completed in ~D ms. Printing state." exec-time)
+       (when (<= qubits 5)
+         (format-log "Amplitudes: ~{~A~^, ~}" (map 'list 'format-complex (amplitudes qvm)))
+         (format-log "Probabilities: ~{~F~^, ~}" (map 'list 'probability (amplitudes qvm))))
+       (format-log "Classical memory (MSB -> LSB): ~v,'0B" (* 8 (classical-memory-size qvm)) (classical-memory qvm)))))
+
+  (uiop:quit))
+
 (defun %main (argv)
   ;; Save the program name away.
   (setf *program-name* (pop argv))
 
-  ;; Check for required arguments.
-  (unless (<= 2 (length argv))
-    (show-help)
-    (uiop:quit))
+  ;; Welcome message.
+  (format-log "Welcome to the Rigetti QVM.")
 
   ;; Run the program.
   (handler-case
-      (command-line-arguments:handle-command-line
-       *option-spec*
-       'process-options
-       :command-line argv
-       :name "qvm"
-       :positional-arity 2
-       :rest-arity nil)
+      (cond
+        ((null argv)
+         (start-server))
+        ((null (cdr argv))
+         (show-help)
+         (uiop:quit))
+        (t
+         (command-line-arguments:handle-command-line
+          *option-spec*
+          'process-options
+          :command-line argv
+          :name "qvm"
+          :positional-arity 2
+          :rest-arity nil)))
     (condition (c)
       (format *error-output* "~&! ! ! Condition raised: ~A~%" c)
       (uiop:quit 1))))
