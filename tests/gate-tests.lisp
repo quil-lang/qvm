d8720bdce0786d158e74a6ea7aedf33c11d99a13
Robert Smith <robert@rigetti.com>
2016-05-20 13:15:17 -0700

    add Bell tests

--- tests/gate-tests.lisp
+++ tests/gate-tests.lisp
@@ -4,6 +4,12 @@
 
 (in-package #:qvm-tests)
 
+(defun vector-first (a)
+  (aref a 0))
+
+(defun vector-last (a)
+  (aref a (1- (length a))))
+
 (deftest test-hadamard ()
   "Test Hadamard initialization on several qubits."
   (flet ((test-size (size)
@@ -20,4 +26,19 @@
   "Test |000> -> |111> inversion."
   (let* ((qil '((NOT 0) (NOT 1) (NOT 2)))
          (qvm (run-program 3 qil)))
-    (is (double-float= 1 (abs (aref (qvm::amplitudes qvm) (1- (expt 2 3)))) 1/10000))))
+    (is (double-float= 1 (probability (vector-last (qvm::amplitudes qvm))) 1/10000))))
+
+(deftest test-bell ()
+  "Test the construction of a Bell pair."
+  (labels ((bell-state (n)
+             "Construct an N-qubit Bell state."
+             (cons
+              '(H 0)
+              (loop :for i :from 1 :below n
+                    :collect `(cnot 0 ,i))))
+           (run-bell (i)
+             (qvm::amplitudes (run-program i (bell-state i)))))
+    (loop :for i :from 1 :to 7 
+          :for amps := (run-bell i)
+          :do (is (double-float= 1/2 (probability (vector-first amps)) 1/10000))
+              (is (double-float= 1/2 (probability (vector-last amps)) 1/10000)))))
