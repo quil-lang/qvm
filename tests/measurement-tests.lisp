636dbafa1eea480515d69bf45c9d784ae88cc58f
Robert Smith <robert@rigetti.com>
2016-07-01 11:44:22 -0700

    add simple measurement test

--- tests/measurement-tests.lisp
+++ tests/measurement-tests.lisp
@@ -15,3 +15,16 @@
     (loop :for i :below (length test-cases)
           :for address :in test-cases
           :do (is (= address (qvm::index-to-address index i))))))
+
+(deftest test-simple-measurements ()
+  "Test that some simple measurements work."
+  (let ((p (with-output-to-quil (s)
+             (write-line "X 0" s)
+             (write-line "X 2" s)
+             (write-line "MEASURE 0 [0]" s)
+             (write-line "MEASURE 1 [1]" s)
+             (write-line "MEASURE 2 [2]" s))))
+    (let ((qvm (qvm:run-program (cl-quil:qubits-needed p) p)))
+      (is (= 1 (qvm::classical-bit qvm 0)))
+      (is (= 0 (qvm::classical-bit qvm 1)))
+      (is (= 1 (qvm::classical-bit qvm 2))))))
