cef4dc59bc26f0d8c3ccf72ae59cf4e7b92d2e90
Robert Smith <robert@rigetti.com>
2016-07-05 23:37:52 -0700

    make bench.lisp more useful for speed testing

--- bench/bench.lisp
+++ bench/bench.lisp
@@ -5,10 +5,20 @@
 (ql:quickload :qvm)
 (ql:quickload :cl-quil)
 
-(defparameter *quil*
-  (let ((cl-quil::*allow-unresolved-applications* t))
-    (cl-quil:read-quil-file "qaoa_8q.quil")))
+(defparameter *quil-file* "H6.quil")
+(defvar *quil* nil)
 
+
+(format t "PARSING========================================~%")
+(sb-sprof:with-profiling (:max-samples 1000
+                          :report :graph
+                          :loop t
+                          :reset t)
+  (setq *quil*
+        (let ((cl-quil::*allow-unresolved-applications* t))
+          (cl-quil:read-quil-file *quil-file*))))
+
+(format t "RUNNING========================================~%")
 (let ((q (qvm:make-qvm (cl-quil:qubits-needed *quil*))))
   (qvm:load-program q *quil*)
   (sb-sprof:with-profiling (:max-samples 1000
@@ -20,7 +30,7 @@
 
   (qvm::reset q)
   (setf (qvm::pc q) 0)
-  
+  #+ignore
   (sb-sprof:with-profiling (:max-samples 10000
                             :report :graph
                             :mode :alloc
