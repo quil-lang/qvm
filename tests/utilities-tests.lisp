d6049fd5e0cebdd0b4b7006407643e7091ab952d
Peter Karalekas <peter@rigetti.com>
2017-01-03 00:28:14 -0500

    Unit tests for nat-tuple operations (#64)

--- tests/utilities-tests.lisp
+++ tests/utilities-tests.lisp
@@ -1,22 +1,57 @@
 ;;;; utilities-tests.lisp
 ;;;;
-;;;; Author: Robert Smith
+;;;; Authors: Robert Smith
+;;;;          Peter Karalekas
 
 (in-package #:qvm-tests)
 
-(deftest test-nat-tuple-operations ()
-  "Test various operations on the NAT-TUPLE data structure."
-  (flet ((members (nt)
-           (let ((membership 0))
-             (qvm::do-nat-tuple (i elt nt)
-               (setf (ldb (byte 1 elt) membership) 1))
-             membership)))
-    (let ((empty (qvm::nat-tuple))
-          (three (qvm::nat-tuple 2 3 4))
-          (two   (qvm::nat-tuple 1 3)))
-      ;; Test NAT-TUPLE-CARDINALITY
-      (is (= 0 (qvm::nat-tuple-cardinality empty)))
-      (is (= 3 (qvm::nat-tuple-cardinality three)))
-      (is (= 2 (qvm::nat-tuple-cardinality two))))))
+(defun set-equal (list1 list2)
+  (and (= (length list1)
+          (length list2))
+       (loop :for item :in list2
+             :always (member item list1))))
 
-;; TODO: test other nat tuple functions
+(deftest test-make-nat-tuple ()
+  (let ((nt (qvm::make-nat-tuple)))
+    (is (null (qvm::nat-tuple-list nt)))
+    (is (= 0 (qvm::nat-tuple-membership nt)))))
+
+(deftest test-nat-tuple-add ()
+  (let* ((nt (qvm::nat-tuple 1 2 3))
+         (elt 4)
+         (ntadd (qvm::nat-tuple-add nt elt))
+         (eltsame 3)
+         (ntsame (qvm::nat-tuple-add nt eltsame)))
+    (is (equal (cons elt (qvm::nat-tuple-list nt))
+               (qvm::nat-tuple-list ntadd)))
+    (is (= (dpb 1 (byte 1 elt) (qvm::nat-tuple-membership nt))
+           (qvm::nat-tuple-membership ntadd)))
+    (is (equal (qvm::nat-tuple-list nt)
+               (qvm::nat-tuple-list ntsame)))
+    (is (= (qvm::nat-tuple-membership nt)
+           (qvm::nat-tuple-membership ntsame)))))
+
+(deftest test-nat-tuple-cardinality ()
+  (let ((empty (qvm::nat-tuple))
+        (three (qvm::nat-tuple 2 3 4))
+        (two   (qvm::nat-tuple 1 3)))
+    (is (= 0 (qvm::nat-tuple-cardinality empty)))
+    (is (= 3 (qvm::nat-tuple-cardinality three)))
+    (is (= 2 (qvm::nat-tuple-cardinality two)))))
+
+(deftest test-nat-tuple ()
+  (let* ((vals (list 1 2 3))
+         (nt (apply #'qvm::nat-tuple vals)))
+    (is (equal (reverse vals) (qvm::nat-tuple-list nt)))
+    (is (every (lambda (bit) (logbitp bit (qvm::nat-tuple-membership nt))) vals))))
+
+(deftest test-nat-tuple-complement ()
+  (let* ((n 6)
+         (nt (qvm::nat-tuple 0 2 4))
+         (ntcomp (qvm::nat-tuple-complement n nt))
+         (ntfull (qvm::nat-tuple-complement n (qvm::nat-tuple))))
+    (is (null (intersection (qvm::nat-tuple-list nt)
+                            (qvm::nat-tuple-list ntcomp))))
+    (is (set-equal (qvm::nat-tuple-list ntfull)
+                   (union (qvm::nat-tuple-list nt)
+                          (qvm::nat-tuple-list ntcomp))))))
