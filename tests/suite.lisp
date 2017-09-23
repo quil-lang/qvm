00359bc9e5aaca6f925571fc630dab9bed48c693
Robert Smith <robert@rigetti.com>
2017-09-23 16:14:15 -0700

    compensate for fiasco bug

--- tests/suite.lisp
+++ tests/suite.lisp
@@ -6,6 +6,9 @@
 
 (defun run-qvm-tests (&key (headless nil))
   "Run all QVM tests. If HEADLESS is T, disable interactive debugging and quit on completion."
+  ;; Bug in Fiasco commit fe89c0e924c22c667cc11c6fc6e79419fc7c1a8b
+  (setf fiasco::*test-run-standard-output* (make-broadcast-stream
+                                            *standard-output*))
   (qvm:prepare-for-parallelization)
   (cond
     ((null headless)
