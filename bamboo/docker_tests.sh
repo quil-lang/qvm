7ecd3e36d65d7b2e3b5937ee35e4ab6e18cfbf2d
Peter Karalekas <peter@rigetti.com>
2017-10-26 01:26:44 -0700

    Add bamboo subdirectory containing files needed to build and run the qvm-tests Docker image for the QVM bamboo build plan

---
+++ bamboo/docker_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+###############################################################################
+# Bamboo script for running QVM tests in the qvm-tests Docker container
+#
+# Steps:
+# 1) Log in to our internal Docker Harbor
+# 2) Build the qvm-tests Docker image
+# 3) Run the qvm-tests Docker image to execute the QVM tests
+# 4) Push the qvm-tests Docker image to the Docker Harbor
+###############################################################################
+set -ex
+
+docker login -u bamboo -p Bamboo1@ docker.lab.rigetti.com
+make -C ../docker qvm-tests
+docker run --name qvm-tests docker.lab.rigetti.com/qcs/qvm-tests
+docker push docker.lab.rigetti.com/qcs/qvm-tests
