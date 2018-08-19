e02d3542ed6e2d67d9e7733d71ca7ef301061bef
Peter Karalekas <peter@rigetti.com>
2018-08-19 14:07:26 -0700

    Remove the bamboo subdirectory

--- bamboo/docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-###############################################################################
-# Bamboo script for building the Docker image(s) defined in the docker Makefile
-###############################################################################
-make -C ../docker
--- bamboo/docker_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-###############################################################################
-# Bamboo script for running the dockerized tests defined in the docker Makefile
-###############################################################################
-make -C ../docker test
