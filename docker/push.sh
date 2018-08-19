37d3cf27914ab1c01a653abb1e8db1d562123ed0
Peter Karalekas <peter@rigetti.com>
2018-08-19 14:07:09 -0700

    Add docker push.sh script and Makefile target

---
+++ docker/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+###############################################################################
+# Script for tagging and pushing Docker images to our internal Harbor
+###############################################################################
+echo "No need to push a tests image, continuing..."
