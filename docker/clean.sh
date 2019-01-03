8b4beb2b22f9102399c3ccf66937c97c2825fd21
Peter Karalekas <peter@rigetti.com>
2019-01-02 16:26:46 -0800

    Resolve issues relating to merging the qvm-app and qvm build infra

--- docker/clean.sh
+++ docker/clean.sh
@@ -8,3 +8,15 @@ docker rm $(docker ps -a -q --filter=ancestor=docker.lab.rigetti.com/qcs/qvm-tes
 docker rm $(docker ps -a -q --filter=ancestor=qvm-tests) || true
 docker rmi docker.lab.rigetti.com/qcs/qvm-tests || true
 docker rmi qvm-tests || true
+
+docker rm -f qvm || true
+docker rm $(docker ps -a -q --filter=ancestor=docker.lab.rigetti.com/qcs/qvm) || true
+docker rm $(docker ps -a -q --filter=ancestor=qvm) || true
+docker rmi docker.lab.rigetti.com/qcs/qvm || true
+docker rmi qvm || true
+
+docker rm -f qvm-base || true
+docker rm $(docker ps -a -q --filter=ancestor=docker.lab.rigetti.com/qcs/qvm-base) || true
+docker rm $(docker ps -a -q --filter=ancestor=qvm-base) || true
+docker rmi docker.lab.rigetti.com/qcs/qvm-base || true
+docker rmi qvm-base || true
--- docker/push.sh
+++ docker/push.sh
@@ -3,4 +3,26 @@ set -ex
 ###############################################################################
 # Script for tagging and pushing Docker images to our internal Harbor
 ###############################################################################
-echo "No need to push a tests image, continuing..."
+GIT_BRANCH_NAME=`git name-rev --name-only HEAD | cut -d~ -f1 | tr "/" "-"`
+if [ "$GIT_BRANCH_NAME" = "master" ] && [ -z "$bamboo_DOCKER_IMAGE_TAG" ]
+then
+    echo "Standard build on master, so using the 'latest' tag"
+    bamboo_DOCKER_IMAGE_TAG="latest"
+elif [ "$GIT_BRANCH_NAME" = "master" ]
+then
+    STABLE="stable"
+    echo "Custom release build on master, so using overridden bamboo tag and updating stable"
+    docker tag qvm docker.lab.rigetti.com/qcs/qvm:$STABLE
+    docker push docker.lab.rigetti.com/qcs/qvm:$STABLE
+elif [ "$GIT_BRANCH_NAME" != "master" ] && [ -z "$bamboo_DOCKER_IMAGE_TAG" ]
+then
+    echo "Standard build on a branch, so using the branch name as tag"
+    bamboo_DOCKER_IMAGE_TAG="$GIT_BRANCH_NAME"
+elif [ "$GIT_BRANCH_NAME" != "master" ]
+then
+    echo "Custom builds on branches not supported, falling back to branch name as tag"
+    bamboo_DOCKER_IMAGE_TAG="$GIT_BRANCH_NAME"
+fi
+echo "Tagging Docker image with: $bamboo_DOCKER_IMAGE_TAG"
+docker tag qvm docker.lab.rigetti.com/qcs/qvm:$bamboo_DOCKER_IMAGE_TAG
+docker push docker.lab.rigetti.com/qcs/qvm:$bamboo_DOCKER_IMAGE_TAG
