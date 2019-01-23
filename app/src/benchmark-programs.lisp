c1cf6db3e6baa8b044b4c77e9d124d94d88410b9
Robert Smith <robert@rigetti.com>
2019-01-23 14:24:41 -0800

    add baseline benchmark

--- app/src/benchmark-programs.lisp
+++ app/src/benchmark-programs.lisp
@@ -36,9 +36,17 @@
 
 (defun perform-benchmark (type num-qubits)
   (check-type num-qubits (integer 1))
-  (if (string-equal type "suite")
-      (qvm-benchmarks:run-benchmarks :verbose t)
-      (let ((p (alexandria:eswitch (type :test #'string-equal)
+  (cond
+    ((string-equal type "suite")
+     (qvm-benchmarks:run-benchmarks :verbose t))
+    ((string-equal type "baseline")
+     (format-log "Computing baseline serial norm timing...")
+     (finish-output)
+     (tg:gc :full t)
+     (format-log "Baseline serial norm timing: ~D ms"
+                 (norm-baseline-timing (qvm::make-vector (expt 2 num-qubits)))))
+    (t
+     (let ((p (alexandria:eswitch (type :test #'string-equal)
                  ("bell" (bell-program num-qubits))
                  ("qft"  (qft-program num-qubits))
                  ("hadamard" (hadamard-program num-qubits))))
@@ -46,13 +54,7 @@
             timing)
         (qvm:load-program q p :supersede-memory-subsystem t)
 
-        (format-log "Computing baseline serial norm timing...")
-        (finish-output)
-
-        (tg:gc :full t)
-        (format-log "Baseline serial norm timing: ~D ms" (norm-baseline-timing (qvm::amplitudes q)))
-
-        (tg:gc :full t)
+                (tg:gc :full t)
 
         (format-log "Starting ~S benchmark with ~D qubits...~%" type num-qubits)
 
@@ -61,4 +63,4 @@
 
         (room)
         (terpri)
-        (format-log "Total time for program run: ~D ms" timing))))
+        (format-log "Total time for program run: ~D ms" timing)))))
