8b4a1f9fb7be31ba6746ac8b1a250114e2f8ac5b
Robert Smith <robert@rigetti.com>
2019-04-17 13:07:00 -0700

    enable gate fusion and measure chain optimization

--- src/qvm.lisp
+++ src/qvm.lisp
@@ -180,6 +180,8 @@ This will not clear previously installed gates from the QVM."
 (defun compile-loaded-program (qvm)
   "Compile the loaded program on the QVM QVM."
   (unless (program-compiled-p qvm)
+    (setf (program qvm) (quil::fuse-gates-in-executable-code (program qvm)))
+    (setf (program qvm) (compile-measure-chains (program qvm) (number-of-qubits qvm)))
     (setf (program qvm)
           (map 'vector (lambda (isn) (compile-instruction qvm isn)) (program qvm)))
     (setf (program-compiled-p qvm) t))
