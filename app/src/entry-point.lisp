bff1b726ec99ba340a77ba1fd75ed9f8f4ee2783
Robert Smith <robert@rigetti.com>
2019-07-10 14:03:20 -0700

    add baseline benchmark, remove computing it by default

--- app/src/entry-point.lisp
+++ app/src/entry-point.lisp
@@ -10,7 +10,7 @@
 (defvar *entered-from-main* nil)
 (defvar *program-name* "qvm")
 
-(defparameter *benchmark-types* '("bell" "qft" "hadamard" "qualcs" #-forest-sdk "suite")
+(defparameter *benchmark-types* '("bell" "qft" "hadamard" "qualcs" "baseline" #-forest-sdk "suite")
   "List of allowed benchmark names.")
 
 (defparameter *available-simulation-methods* '("pure-state" "full-density-matrix")
