84a78dc9e1b4f5aa895a04137ea0033a5c9f0030
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-08-03 15:24:17 -0500

    Add WITH-LOCKED-LOG around calls to CL-SYSLOG
    This cherry-picks part of the diff from #111, adding a few more
    WITH-LOCKED-LOG to a handful of places that were logging to syslog
    directly, without using the FORMAT-LOG macro.
    
    Fixes #110
    Fixes rigetti/pyquil#967
    
    [1] https://github.com/rigetti/qvm/pull/111
    [2] https://github.com/rigetti/pyquil/issues/967

--- app/src/utilities.lisp
+++ app/src/utilities.lisp
@@ -102,6 +102,11 @@
               (tbnl:session-remote-addr tbnl:*session*)
               (tbnl:session-id tbnl:*session*))))
 
+(global-vars:define-global-var **log-lock** (bt:make-lock "Log Lock"))
+(defmacro with-locked-log (() &body body)
+  `(bt:with-lock-held (**log-lock**)
+     ,@body))
+
 (defmacro format-log (level-or-fmt-string &rest fmt-string-or-args)
   "Send a message to syslog. If the first argument LEVEL-OR-FMT-STRING is a
 keyword it is assumed to be a non-default log level (:debug), otherwise it is a control
@@ -111,16 +116,18 @@ string followed by optional args (as in FORMAT)."
     ;; time.
     (cl-syslog:get-priority level-or-fmt-string))
   (if (keywordp level-or-fmt-string)
-      `(cl-syslog:format-log
-        *logger*
-        ',level-or-fmt-string
-        "~A~@?"
-        (session-info)
-        ,@fmt-string-or-args)
-      `(cl-syslog:format-log
-        *logger*
-        ':debug
-        "~A~@?"
-        (session-info)
-        ,level-or-fmt-string
-        ,@fmt-string-or-args)))
+      `(with-locked-log ()
+         (cl-syslog:format-log
+          *logger*
+          ',level-or-fmt-string
+          "~A~@?"
+          (session-info)
+          ,@fmt-string-or-args))
+      `(with-locked-log ()
+         (cl-syslog:format-log
+          *logger*
+          ':debug
+          "~A~@?"
+          (session-info)
+          ,level-or-fmt-string
+          ,@fmt-string-or-args))))
