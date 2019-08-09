66ecf707846b72ace9b749ffde49a222d40b4524
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-08-09 13:54:59 -0500

    Simplify RUN-QVM-APP-NG-TESTS

--- app-ng/tests/suite.lisp
+++ app-ng/tests/suite.lisp
@@ -3,19 +3,13 @@
 (defun run-qvm-app-ng-tests (&key (verbose nil) (headless nil))
   "Run all qvm-app-ng tests. If VERBOSE is T, print out lots of test info. If HEADLESS is T, disable interactive debugging and quit on completion."
   (setf fiasco::*test-run-standard-output* (make-broadcast-stream *standard-output*))
-  (cond
-    ((null headless)
-     (run-package-tests :package ':qvm-app-ng-tests
-                        :verbose verbose
-                        :describe-failures t
-                        :interactive t))
-    (t
-     (let ((successp (run-package-tests :package ':qvm-app-ng-tests
-                                        :verbose t
-                                        :describe-failures t
-                                        :interactive nil)))
-       (uiop:quit (if successp 0 1))))))
-
+  (let ((successp (run-package-tests :package ':qvm-app-ng-tests
+                                     :verbose (or headless verbose)
+                                     :describe-failures t
+                                     :interactive (not headless))))
+    (if headless
+        (quit-nicely (qvm::boolean-bit successp))
+        successp)))
 
 (deftest test-update-available ()
   (multiple-value-bind (update-available-p update)
@@ -25,5 +19,3 @@
         ;; we don't want to error in that case. Skip instead.
         (is update)
         (skip))))
-
-
