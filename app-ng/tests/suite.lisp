cd9c5e7e39b7a34eb7b04976a87e5496fb0f746a
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-08-12 13:52:34 -0500

    Minor cleanup/refactoring of RUN-QVM-APP-NG-TESTS

--- app-ng/tests/suite.lisp
+++ app-ng/tests/suite.lisp
@@ -3,19 +3,13 @@
 (defun run-qvm-app-ng-tests (&key (verbose nil) (headless nil))
   "Run all qvm-app-ng tests. If VERBOSE is T, print out lots of test info. If HEADLESS is T, disable interactive debugging and quit on completion."
   (setf fiasco::*test-run-standard-output* (make-broadcast-stream *standard-output*))
-  (cond
-    ((null headless)
-     (run-package-tests :package ':qvm-app-ng-tests
-                        :verbose verbose
-                        :describe-failures t
-                        :interactive t))
-    (t
-     (let ((successp (run-package-tests :package ':qvm-app-ng-tests
-                                        :verbose t
-                                        :describe-failures t
-                                        :interactive nil)))
-       (uiop:quit (if successp 0 1))))))
-
+  (let ((successp (run-package-tests :package ':qvm-app-ng-tests
+                                     :verbose (or headless verbose)
+                                     :describe-failures t
+                                     :interactive (not headless))))
+    (if headless
+        (qvm-app-ng::quit-nicely successp)
+        successp)))
 
 (deftest test-update-available ()
   (multiple-value-bind (update-available-p update)
