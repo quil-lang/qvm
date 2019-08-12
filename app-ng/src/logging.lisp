748c1b41244a9e8d61f470efddd9be4dc29bd5b2
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-08-12 12:57:50 -0500

    Logging fixes
    - Remove duplicate definition of FORMAT-LOG
    - Apply WITH-LOCKED-LOG and STYLE-WARNING fixes from 7ecb81e and
      4b1f74d to app-ng sources.

--- app-ng/src/logging.lisp
+++ app-ng/src/logging.lisp
@@ -21,26 +21,35 @@
                                  (cl-syslog:syslog-log-writer *program-name* :local0)
                                  *error-output*))))
 
+(global-vars:define-global-var **log-lock** (bt:make-lock "Log Lock"))
+(defmacro with-locked-log (() &body body)
+  `(bt:with-lock-held (**log-lock**)
+     ,@body))
+
 (defmacro format-log (level-or-fmt-string &rest fmt-string-or-args)
-  "Send a message to syslog. If the first argument LEVEL-OR-FMT-STRING is a keyword it is assumed to be a non-default log level (:debug), otherwise it is a control string followed by optional args (as in FORMAT)."
+  "Send a message to syslog. If the first argument LEVEL-OR-FMT-STRING is a
+keyword it is assumed to be a non-default log level (:debug), otherwise it is a control
+string followed by optional args (as in FORMAT)."
   (when (keywordp level-or-fmt-string)
     ;; Sanity check that it's a valid log level at macroexpansion
     ;; time.
     (cl-syslog:get-priority level-or-fmt-string))
   (if (keywordp level-or-fmt-string)
-      `(cl-syslog:format-log
-        *logger*
-        ',level-or-fmt-string
-        "~A~@?"
-        (session-info)
-        ,@fmt-string-or-args)
-      `(cl-syslog:format-log
-        *logger*
-        ':debug
-        "~A~@?"
-        (session-info)
-        ,level-or-fmt-string
-        ,@fmt-string-or-args)))
+      `(with-locked-log ()
+         (cl-syslog:format-log
+          *logger*
+          ',level-or-fmt-string
+          "~A~@?"
+          (session-info)
+          ,@fmt-string-or-args))
+      `(with-locked-log ()
+         (cl-syslog:format-log
+          *logger*
+          ':debug
+          "~A~@?"
+          (session-info)
+          ,level-or-fmt-string
+          ,@fmt-string-or-args))))
 
 (defun log-level-string-to-symbol (log-level)
   "Match the LOG-LEVEL string to one CL-SYSLOG's prority symbols."
