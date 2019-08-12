36ef6f6de9bb29fd0a7136f8347b72dc2d19b3fe
Zach Beane <zach@rigetti.com>
2019-08-12 10:13:07 -0400

    Make FORMAT-LOG a function and simplify it a bit.

--- app/src/utilities.lisp
+++ app/src/utilities.lisp
@@ -107,27 +107,27 @@
   `(bt:with-lock-held (**log-lock**)
      ,@body))
 
-(defmacro format-log (level-or-fmt-string &rest fmt-string-or-args)
-  "Send a message to syslog. If the first argument LEVEL-OR-FMT-STRING is a
-keyword it is assumed to be a non-default log level (:debug), otherwise it is a control
-string followed by optional args (as in FORMAT)."
-  (when (keywordp level-or-fmt-string)
-    ;; Sanity check that it's a valid log level at macroexpansion
+(defun format-log (&rest args)
+  "Send a message to syslog. If the first argument of ARGS is a
+keyword it is assumed to be a non-default log level (:debug),
+otherwise it is a control string followed by optional args (as in
+FORMAT)."
+  (destructuring-bind (level format-control &rest format-args)
+      (if (keywordp (first args))
+          args
+          (list* ':debug args))
+    (with-locked-log ()
+      (cl-syslog:format-log
+       *logger*
+       level
+       "~A~?"
+       (session-info)
+       format-control
+       format-args))))
+
+(define-compiler-macro format-log (&whole whole &rest args)
+  (when (keywordp (first args))
+    ;; Sanity check that it's a valid log level at compile
     ;; time.
-    (cl-syslog:get-priority level-or-fmt-string))
-  (if (keywordp level-or-fmt-string)
-      `(with-locked-log ()
-         (cl-syslog:format-log
-          *logger*
-          ',level-or-fmt-string
-          "~A~@?"
-          (session-info)
-          ,@fmt-string-or-args))
-      `(with-locked-log ()
-         (cl-syslog:format-log
-          *logger*
-          ':debug
-          "~A~@?"
-          (session-info)
-          ,level-or-fmt-string
-          ,@fmt-string-or-args))))
+    (cl-syslog:get-priority (first args)))
+  whole)
