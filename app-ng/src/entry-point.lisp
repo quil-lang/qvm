d20ccd2888f6ee549e424e2dd9cf895a1f06a0a5
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-10-02 12:39:07 -0500

    Command line options + support for running an rpc request on startup
    - Add some basic command line options (help, version, log-level, etc).
    
    - Add support for running an rpc request immediately on application
      startup via an --rpc-request command line flag. This requirement
      comes from an internal user request.
    
    - Write informational messages like SHOW-{HELP,VERSION,WELCOME}
      on *ERROR-OUTPUT* rather than *STANDARD-OUTPUT*. This makes it
      easier for the caller to read the RPC result for the initial
      --rpc-request from *STANDARD-OUTPUT*
    
    Closes #191

--- app-ng/src/entry-point.lisp
+++ app-ng/src/entry-point.lisp
@@ -4,30 +4,103 @@
 
 (in-package #:qvm-app-ng)
 
-(defparameter *option-spec* '())
+(defparameter *option-spec*
+  `((("help" #\h)
+     :type boolean
+     :optional t
+     :documentation "Display help")
+    (("host")
+     :type string
+     :initial-value ,+default-server-address+
+     :documentation "Host on which to start the QVM server")
+    (("log-level")
+     :type string
+     :initial-value "debug"
+     :documentation ,(format nil "Maximum logging level. Must be one of ~{~(~A~)~^, ~}"
+                             +available-log-levels+))
+    (("port" #\p)
+     :type integer
+     :initial-value ,+default-server-port+
+     :documentation "Port to start the QVM server on")
+    (("rpc-request")
+     :type string
+     :initial-value nil
+     :documentation "An initial RPC request to execute on application startup. The value should be a JSON object that corresponds to the request body you would send to invoke the RPC method via the HTTP server. For example, to invoke the \"version\" method, pass '{\"type\":\"version\"}'. The result will printed on *STANDARD-OUTPUT*.")
+    (("server" #\S)
+     :type boolean
+     :optional t
+     :initial-value nil
+     :documentation "Start a QVM server")
+    (("verbose")
+     :type integer
+     :initial-value 1
+     :documentation "Desired verbosity level. The following levels are understood.
+
+> 1: verbose output
+= 1: normal logging and output (default)
+< 1: no logging and only essential output")
+    (("version" #\v)
+     :type boolean
+     :optional t
+     :documentation "Display the versions of the app and underlying QVM")))
+
+(defun process-options (&key
+                          help
+                          host
+                          log-level
+                          port
+                          rpc-request
+                          server
+                          verbose
+                          version)
+
+  (when help
+    (show-help)
+    (return-from process-options))
+
+  (when version
+    (show-version)
+    (return-from process-options))
+
+  (when (> verbose 0)
+    (setf *logger* (make-logger "qvm-ng" (parse-log-level log-level)))
+    (show-welcome))
+
+  (when rpc-request
+    (run-initial-rpc-request rpc-request))
+
+  (when server
+    (start-server-mode :host host :port port)
+    #|not reached|#))
 
 (defun show-help ()
-  (format t "Usage:~%")
-  (format t "    qvm-ng [<options>...]~%~%")
-  (format t "Options:~%")
-  (command-line-arguments:show-option-help *option-spec* :sort-names t))
+  (format *error-output* "Usage:~%")
+  (format *error-output* "    qvm-ng [<options>...]~%~%")
+  (format *error-output* "Options:~%")
+  (command-line-arguments:show-option-help *option-spec* :stream *error-output* :sort-names t))
 
 (defun show-version ()
-  (format t "~A [~A]~%" +QVM-VERSION+ +GIT-HASH+))
+  (format *error-output* "~A [~A]~%" +QVM-VERSION+ +GIT-HASH+))
 
 (defun show-welcome ()
-  (format t "~&~
-*****************************************~%~
-* Welcome to the Rigetti QVM (Next Gen) *~%~
-*****************************************~%~
-Copyright (c) 2016-2019 Rigetti Computing.~2%")
-  (format t "(Configured with ~A MiB of workspace and ~D core~:P.)~2%"
+  (format *error-output*
+          "~&*****************************************~%~
+             * Welcome to the Rigetti QVM (Next Gen) *~%~
+             *****************************************~%~
+             Copyright (c) 2016-2019 Rigetti Computing.~2%~
+             (Configured with ~A MiB of workspace and ~D core~:P.)~2%"
           #+sbcl
           (floor (sb-ext:dynamic-space-size) (expt 1024 2))
           #-sbcl
           "many many"
           (max 1 (qvm:count-logical-cores)))
   nil)
+
+(defun run-initial-rpc-request (rpc-request)
+  (let ((*request-json* (parse-json-or-lose rpc-request)))
+    (alexandria:if-let ((handler (lookup-rpc-handler-for-request *request-json*)))
+      (format t "~A~%" (funcall handler))
+      (error "No handler found for initial rpc request: ~S" rpc-request))))
 
 (defun generalized-boolean-to-exit-code (successp)
   (cond ((integerp successp) successp)
@@ -41,24 +114,34 @@ Copyright (c) 2016-2019 Rigetti Computing.~2%")
   #-sbcl
   (uiop:quit code t))
 
-(defun %main (argv)
-  (declare (ignore argv))
+(defun %entry-point (argv)
+  ;; A (somewhat) testable version of ENTRY-POINT that doesn't QUIT-NICELY or trap errors. Note that
+  ;; certain flags in ARGV (e.g. "-S") will cause PROCESS-OPTIONS to start a server and never return.
+  (command-line-arguments:handle-command-line
+   *option-spec*
+   'process-options
+   :command-line argv
+   :name "qvm-ng"
+   :rest-arity nil))
+
+(defun entry-point (argv)
+  "The entry point for running the application from the command line."
   (handler-case
       (progn
-        (show-welcome)
-        (setf *logger* (make-logger "qvm-ng" ':debug))
-        (start-server-mode :host +default-server-address+ :port +default-server-port+)
+        (%entry-point argv)
         (quit-nicely 0))
     #+sbcl
     (sb-sys:interactive-interrupt (c)
       (declare (ignore c))
       (format-log "Caught Control-C. Quitting.")
       (quit-nicely 0))
+    ;; TODO(appleby): re-enable this before shipping (maybe).
+    #+(or)
     (error (c)
       (format *error-output* "~&! ! ! Condition raised: ~A~%" c)
       (format-log :err "Error encountered, quitting.")
       (quit-nicely 1))))
 
 (defun asdf-entry-point ()
-  (%main (uiop:command-line-arguments)))
+  (entry-point (uiop:command-line-arguments)))
 
