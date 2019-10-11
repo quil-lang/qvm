d1bacea42eb64b9147637c517749abe7f743b116
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-10-11 10:36:14 -0500

    Move app-ng UUID stuff into a separate file
    This is in preparation for job queue handling, which will also want to
    use UUIDs for job IDs.

--- app-ng/tests/test-validators.lisp
+++ app-ng/tests/test-validators.lisp
@@ -1,20 +1,20 @@
 (in-package :qvm-app-ng-tests)
 
-(deftest test-valid-persistent-qvm-token-p ()
-  (is (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-4eef-8777-de4c5057aac9"))
-  (is (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-4eef-9777-de4c5057aac9"))
-  (is (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-4eef-a777-de4c5057aac9"))
-  (is (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-4eef-b777-de4c5057aac9"))
-  (is (qvm-app-ng::valid-persistent-qvm-token-p (qvm-app-ng::make-persistent-qvm-token)))
+(deftest test-valid-uuid-string-p ()
+  (is (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-4eef-8777-de4c5057aac9"))
+  (is (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-4eef-9777-de4c5057aac9"))
+  (is (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-4eef-a777-de4c5057aac9"))
+  (is (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-4eef-b777-de4c5057aac9"))
+  (is (qvm-app-ng::valid-uuid-string-p (qvm-app-ng::make-uuid-string)))
 
   ;; uppercase rejected
-  (is (not (qvm-app-ng::valid-persistent-qvm-token-p "FA8ACF54-201D-4EEF-9777-DE4C5057AAC9")))
+  (is (not (qvm-app-ng::valid-uuid-string-p "FA8ACF54-201D-4EEF-9777-DE4C5057AAC9")))
 
   ;; invalid version field
-  (is (not (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-5eef-9777-de4c5057aac9")))
+  (is (not (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-5eef-9777-de4c5057aac9")))
 
   ;; invalid clock sequence high-order bits
-  (is (not (qvm-app-ng::valid-persistent-qvm-token-p "fa8acf54-201d-4eef-7777-de4c5057aac9"))))
+  (is (not (qvm-app-ng::valid-uuid-string-p "fa8acf54-201d-4eef-7777-de4c5057aac9"))))
 
 (deftest test-parse-simualtion-method ()
   (is (eq 'qvm-app-ng::pure-state (qvm-app-ng::parse-simulation-method "pure-state")))
