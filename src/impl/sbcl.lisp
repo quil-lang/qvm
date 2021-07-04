e2b0dd016ac7cec4f7dd83838cfac222aded95d7
Mark Skilbeck <mark.skilbeck@rigetti.com>
2021-07-04 15:38:57 +0100

    redefine code removed by static-vectors
    .

--- src/impl/sbcl.lisp
+++ src/impl/sbcl.lisp
@@ -9,12 +9,33 @@
 (defun shm-vector-header-size ()
   (* sb-vm:vector-data-offset sb-vm:n-word-bytes))
 
+;; +array-header-size+ and %allocation-size were originally in
+;; static-vectors but commit c85d2826955c0a8b7ffce87b270687cb8d3ed254
+;; removed them. Depending on not-exported functions is bad for this
+;; reason.
+(defconstant +array-header-size+
+  (* sb-vm:vector-data-offset sb-vm:n-word-bytes))
+
+(defun %allocation-size (length widetag n-bits)
+  (flet ((string-widetag-p (widetag)
+           (or (= widetag sb-vm:simple-base-string-widetag)
+               #+sb-unicode
+               (= widetag sb-vm:simple-character-string-widetag))))
+    (+ (* 2 sb-vm:n-word-bytes
+          (ceiling
+           (* (if (string-widetag-p widetag)
+                  (1+ length)           ; for the final #\Null
+                  length)
+              n-bits)
+           (* 2 sb-vm:n-word-bits)))
+       (shm-vector-header-size))))
+
 (defun shm-vector-allocation-size (num-elements element-type)
   "Return the size, in octets, needed to store a simple-array of
 NUM-ELEMENTS ELEMENT-TYPE objects."
   (multiple-value-bind (widetag n-bits)
       (static-vectors::vector-widetag-and-n-bits element-type)
-    (static-vectors::%allocation-size num-elements widetag n-bits)))
+    (%allocation-size num-elements widetag n-bits)))
 
 (defun shm-vector-from-pointer (pointer num-elements element-type)
   "Return two values: a specialized vector of NUM-ELEMENTS for
@@ -23,7 +44,10 @@ clean up that vector."
   (multiple-value-bind (widetag n-bits)
       (static-vectors::vector-widetag-and-n-bits element-type)
     (declare (ignore n-bits))
-    (values (static-vectors::vector-from-pointer pointer widetag num-elements)
-            ;; No finalizer is needed for the vector itself in SBCL.
-            (lambda () nil))))
-
+    (setf (sb-sys:sap-ref-word pointer 0) widetag
+          (sb-sys:sap-ref-word pointer sb-vm:n-word-bytes) (sb-vm:fixnumize num-elements))
+    (values
+     (sb-kernel:%make-lisp-obj (logior (cffi:pointer-address pointer)
+                                       sb-vm:other-pointer-lowtag))
+     ;; No finalizer is needed for the vector itself in SBCL.)
+     (lambda () nil))))
